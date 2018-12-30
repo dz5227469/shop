b157101b6c8f8b13f19fcbbad128f6b59585f291
dz5227469 <349619016@qq.com>
2018-12-30 14:35:16 +0800

    修改

--- hlsmms/src/main.js
+++ hlsmms/src/main.js
@@ -6,6 +6,22 @@ import './plugins/element.js'
 
 //引入公共样式im
 import './assets/styles/public.css'
+//引入axios
+import axios from 'axios'
+//把axios挂载在vue的原型上，vue实例中就可以无限使用（页面组件中去使用）
+Vue.prototype.axios = axios;
+//引入qs处理axios的post参数
+import qs from 'qs'
+//挂载原型方便后期的vue实例使用
+Vue.prototype.qs = qs;
+
+
+
+
+
+
+
+
 
 Vue.config.productionTip = false
 
--- hlsmms/src/views/userManage.vue
+++ hlsmms/src/views/userManage.vue
@@ -1,48 +1,141 @@
 <template>
   <el-container id="home">
     <!-- 左侧 -->
-    <left></left>
+    <LeftMenu></LeftMenu>
     <!-- 右侧 -->
-    <el-container id="buju">
+    <el-container id="mainContent">
       <!-- 顶部 -->
-      <RightHeader></RightHeader>
+      <RightTop></RightTop>
       <!-- 中间 -->
       <el-main>
         <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <h3>账号管理</h3>
+          <div
+            slot="header"
+            class="clearfix"
+          >
+            <h3>用户管理</h3>
           </div>
-
-          <div id="main">
-            <!-- 模板的内容 -->
+          <div class="text item">
+            <el-table
+              :data="tableData"
+              style="width: 100%"
+            >
+              <el-table-column
+                label="日期"
+                width="180"
+              >
+                <template slot-scope="scope">
+                  <i class="el-icon-time"></i>
+                  <span style="margin-left: 10px">{{ scope.row.date }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column
+                label="姓名"
+                width="180"
+              >
+                <template slot-scope="scope">
+                  <el-popover
+                    trigger="hover"
+                    placement="top"
+                  >
+                    <p>姓名: {{ scope.row.name }}</p>
+                    <p>住址: {{ scope.row.address }}</p>
+                    <div
+                      slot="reference"
+                      class="name-wrapper"
+                    >
+                      <el-tag size="medium">{{ scope.row.name }}</el-tag>
+                    </div>
+                  </el-popover>
+                </template>
+              </el-table-column>
+              <el-table-column label="操作">
+                <template slot-scope="scope">
+                  <el-button
+                    size="mini"
+                    @click="handleEdit(scope.$index, scope.row)"
+                  ><i class="el-icon-edit"></i> 编辑</el-button>
+                  <el-button
+                    size="mini"
+                    type="danger"
+                    @click="handleDelete(scope.$index, scope.row)"
+                  ><i class="el-icon-delete"></i> 删除</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
           </div>
         </el-card>
       </el-main>
       <!-- 底部 -->
-      <foot></foot>
+      <RightBottom></RightBottom>
     </el-container>
   </el-container>
 </template>
 
 <script>
-//ying ru导航组件
-import left from "../components/left";
-import RightHeader from "../components/RightHeader";
-import foot from "../components/foot";
+//引入导航组件
+import LeftMenu from "../components/leftMenu";
+import RightTop from "../components/rightTop";
+import RightBottom from "../components/rightBottom";
 
 export default {
-  // 暴露一个模块
   data() {
-    return {};
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
+    };
   },
   components: {
+    //注册组件
     left,
     RightHeader,
     foot
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    }
   }
 };
 </script>
 
 
-<style >
+<style>
+/* scoped 作用域限定样式只在当前组件生效 */
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
